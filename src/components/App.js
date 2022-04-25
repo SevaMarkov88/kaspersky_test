629e79d08e3fd6ae31db831d73a11adf5d6406f3
SevaMarkov88 <vsevolodmarkoff@yandex.ru>
2022-04-25 22:52:49 +0300

    optimal-block ready

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import FirstSection from "./FirstSection";
+import OptimalBlock from "./OptimalBlock";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <FirstSection />
+      <OptimalBlock />
     </div>
   );
 }
